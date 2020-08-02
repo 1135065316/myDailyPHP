57e44358d7cb30157cda56f29652efd2f8c8ad41
blkZ <1135065316@qq.com>
2020-08-02 21:33:14 +0800

    增加了保存记录以及查询记录的方法

---
+++ saveRecord.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = 'mysql';
+
+$record_time = $_GET["record_time"];
+$is_survival = $_GET["is_survival"];
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO my_daily (record_time, is_survival)
+VALUES (".$record_time.", ".$is_survival.")";
+
+if ($conn->query($sql) === TRUE) {
+	echo "新记录插入成功";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
---
+++ selectRecord.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = 'mysql';
+
+$record_time = $_GET["record_time"];
+ 
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM my_daily where record_time=".$record_time;
+$result = $conn->query($sql);
+ 
+// if ($result->num_rows > 0) {
+// 	$arr=array();
+//     // 输出数据
+//     while($row = $result->fetch_assoc()) {
+// 		$arr[] = $row;
+//     }
+// 	echo json_encode($arr);
+// } else {
+//     echo "0 结果";
+// }
+echo $result;
